08d32db64070c439bc13c692905d8c6a25d0c444
Nick Kotte <32132920+NickKotte@users.noreply.github.com>
2019-06-06 18:00:38 -0400

    Update visualization.js

--- visualization.js
+++ visualization.js
@@ -9,7 +9,7 @@ var drops = [];
 var previous;
 function preload()
 {
-  song = loadSound("./music/Shipwreck.mp3", loaded);
+  song = loadSound("Online-Music-Visualizer/music/Shipwreck.mp3", loaded);
 }
 
 function setup()
